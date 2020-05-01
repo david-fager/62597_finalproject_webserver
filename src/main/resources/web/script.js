f9adacb15cfec6cdfa1a8eb7c8ff6f9b18b26252
Marina <marina@mallingnet.dk>
2020-05-01 17:13:57 +0200

    current status

--- src/main/resources/web/script.js
+++ src/main/resources/web/script.js
@@ -5,7 +5,7 @@ $(function () {
 
         $("#hej").append("" +
             "<div class=\"item\">\n" +
-            "    <img class=\"box\" src=\"images/box.png\" height=\"80\">\n" +
+            "    <img class=\"box\" src=\"images/box.png\" height=\"70\">\n" +
             "    <img class=\"status\" src=\"images/green.png\">\n" +
             "    <p class=\"text\">Tomater</p>\n" +
             "    <p class=\"amount\">x2</p>\n" +
@@ -14,6 +14,12 @@ $(function () {
 
     });
 
+    $("#slider-right").click(function () {
+        $("#box-right").animate({width:'toggle'},350);
+    });
 
+    $("#slider-left").click(function () {
+        $("#box-left").animate({width:'toggle'},350);
+    });
 
 });
