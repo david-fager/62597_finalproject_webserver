fa2b688ead1e115d94aeb4549e30a9125a09d4f0
s185120 <s185120@student.dtu.dk>
2020-05-02 12:53:55 +0200

    Grid layout virker godt

--- src/main/resources/web/script.js
+++ src/main/resources/web/script.js
@@ -3,15 +3,15 @@ $(function () {
     $("#button-new-box").click(function () {
         console.log("Tilføjet boks!")
 
-        $("#items").append("" +
+        $(".grid").append("" +
             "<div class=\"item\">\n" +
             "    <img class=\"box-visual\" src=\"images/box.png\">\n" +
             "    <img class=\"box-status\" src=\"images/green.png\">\n" +
             "    <div class=\"box-content\">\n" +
             "        <div class=\"content-text\">\n" +
-            "            <p>Tomater</p>\n" +
-            "            <p>x2</p>\n" +
-            "            <p>23/04/20</p>\n" +
+            "            <p>PLACEHOLDER</p>\n" +
+            "            <p>999 tilbage</p>\n" +
+            "            <p>Udl.: 00/00/00</p>\n" +
             "        </div>\n" +
             "    </div>\n" +
             "</div>");
