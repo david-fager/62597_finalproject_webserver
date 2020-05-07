ceecfb407b9d6ec6dd1bb5e75e6e6f693905b369
Marina <marina@mallingnet.dk>
2020-05-07 15:14:51 +0200

    current status - RMI

--- src/main/java/Javalin/Server.java
+++ src/main/java/Javalin/Server.java
@@ -1,10 +1,20 @@
 package Javalin;
 
 import io.javalin.Javalin;
+import org.eclipse.jetty.http.HttpStatus;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Server {
     private final boolean DEBUGGING = true; // Set whether to debug or not here
     private Javalin app = null;
+
+    private DateFormat df = new SimpleDateFormat("[dd-MM-yyyy HH:mm:ss] ");
+    private String getTime() {
+        return df.format(Calendar.getInstance().getTimeInMillis());
+    }
 
     public void initialize() {
         javalinSetup();
@@ -23,6 +33,64 @@ public class Server {
     }
 
     private void loginPaths() {
+        app.get("/", context -> {
+            context.redirect("/login");
+        });
+
+        app.get("/login", context -> {
+                    context.render("webapp/login.html");
+        });
+
+        app.get("/login/:username", context -> {
+            javaprogram.informConnect();
+
+            String username = context.pathParam("username");
+            String password = context.queryParam("password");
+
+            boolean success = javaprogram.login(username, password);
+            if (success) {
+                System.out.println(getTime() + "Login success");
+                context.status(HttpStatus.ACCEPTED_202);
+                context.render("webapp/indes.html");
+            } else {
+                System.out.println(getTime() + "Login failed");
+                context.status(HttpStatus.UNAUTHORIZED_401);
+            }
+
+        });
+
+        app.get("/login/forgot", context -> {
+            context.render("webapp/index.html");
+        });
+
+        app.get("/login/forgot/:username", context -> {
+            String username = context.pathParam("username");
+            String message = context.queryParam("message");
+            if (message == null) {
+                message = "";
+            }
+
+            boolean success = javaprogram.forgotPassword(username, message);
+            if (success) {
+                context.status(HttpStatus.OK_200);
+            } else {
+                context.status(HttpStatus.SERVICE_UNAVAILABLE_503);
+            }
+        });
+
+        app.get("/account/changePassword/:oldPassword", context -> {
+            context.status(HttpStatus.UNAUTHORIZED_401).result("<h1>401 Unauthorized</h1>You are not authorized to see this page.").contentType("text/html");
+
+            String oldPassword = context.pathParam("oldPassword");
+            String newPassword = context.queryParam("newPassword");
+
+            Bruger bruger = javaprogram.changePassword(oldPassword, newPassword);
+            if (bruger != null) {
+                context.status(HttpStatus.ACCEPTED_202);
+            } else {
+                context.status(HttpStatus.SERVICE_UNAVAILABLE_503);
+            }
+        });
 
     }
 
